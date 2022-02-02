f2f838a81e2a144a6e59835498d10285c3a7cdcd
n-koenig <nils.koenig01@gmail.com>
2022-02-02 21:49:03 +0100

    die ungefilterte transparente wahrheit

--- plot_matrices.py
+++ plot_matrices.py
@@ -18,20 +18,19 @@ def f1_scores_from_conf_mat(cm):
 
 
 # define and load current benchmark values
-current = 'model_2'
-sum_conf = np.zeros((11, 11))
-for i in range(10):
-    with open(f'benchmarks/{current}_{i+1}.npy', 'rb') as f:
-        # print(sum_conf, np.load(f))
-        conf = np.load(f)
-        print(conf)
-        sum_conf += conf
+current = 'normalize_equalize'
+# sum_conf = np.zeros((11, 11))
+# for i in range(10):
+#     with open(f'benchmarks/{current}_{i+1}.npy', 'rb') as f:
+#         conf = np.load(f)
+#         print(conf)
+#         sum_conf += conf
+# conf_mat = sum_conf / 10
 
-conf_mat = sum_conf / 10
 
-# conf_mat = []
-# with open(f'benchmarks/{current}.npy', 'rb') as f:
-#     conf_mat = np.load(f)
+conf_mat = []
+with open(f'benchmarks/{current}.npy', 'rb') as f:
+    conf_mat = np.load(f)
 
 with open(f'benchmarks/baseline.npy', 'rb') as f:
     base_conf_mat = np.load(f)
@@ -47,8 +46,8 @@ with open(f'benchmarks/baseline.npy', 'rb') as f:
 # compute and save f1 scores
 f1_scores = f1_scores_from_conf_mat(conf_mat)
 f1_scores.append(np.mean(f1_scores))
-# np.savetxt(f'benchmarks/{current}_f1.txt', f1_scores, "%.2f", delimiter='\n')
-
+np.savetxt(f'benchmarks/{current}_f1.txt', f1_scores, "%.2f", delimiter='\n')
+exit()
 # with relation to baseline
 base_f1_scores = f1_scores_from_conf_mat(base_conf_mat)
 base_f1_scores.append(np.mean(base_f1_scores))
